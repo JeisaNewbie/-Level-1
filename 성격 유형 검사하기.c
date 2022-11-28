5f901eb9b8e7e25cb2bb137fc9dfb18a85f95dd0
Je Hwang <88884947+JeisaNewbie@users.noreply.github.com>
2022-11-28 09:41:13 +0900

    Update 성격 유형 검사하기.c

--- 성격 유형 검사하기.c
+++ 성격 유형 검사하기.c
@@ -2,54 +2,66 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int return_point (const char *characters, int *choice)
+{
+    int     negative[3] = {-3, -2, -1};
+    if (characters[0] + characters[1] == 'R' + 'T')
+    {
+        if (!(*choice / 4))
+            *choice = negative[*choice - 1];
+        if (characters[0] == 'T')
+            *choice *= -1;
+        return (0);
+    }
+    else if (characters[0] + characters[1] == 'C' + 'F')
+    {
+        if (!(*choice / 4))
+            *choice = negative[*choice - 1];
+        if (characters[0] == 'F')
+            *choice *= -1;
+        return (1);
+    }
+    else if (characters[0] + characters[1] == 'J' + 'M')
+    {
+        if (!(*choice / 4))
+            *choice = negative[*choice - 1];
+        if (characters[0] == 'M')
+            *choice *= -1;
+        return (2);
+    }
+    else if (characters[0] + characters[1] == 'A' + 'N')
+    {
+        if (!(*choice / 4))
+            *choice = negative[*choice - 1];
+        if (characters[0] == 'N')
+            *choice *= -1;
+        return (3);
+    }
+    else 
+        return (4);
+}
 char* solution(const char* survey[], size_t survey_len, int choices[], size_t choices_len) {
     char*   answer = (char*)malloc(sizeof (char) * 5);
     char    character[9] = "RCJATFMN";
-    int     negative[3] = {-3, -2, -1};
     int     point[4] = {0,};
+    int     j;
 
     if (survey_len >= 1001 || choices_len >= 1001 || !(survey_len == choices_len))
         return ("Length Over");
     if (survey_len == 0 || choices_len == 0)
         return ("0 Length");
-    for (int i = 0; i < survey_len; i++)
+    for (size_t i = 0; i < survey_len; i++)
     {
         if (choices[i] < 1 || choices[i] > 7)
-            return ("Incorrect Choise"); 
-        if (survey[i][0] + survey[i][1] == 'R' + 'T')
-        {
-            if (!(choices[i] / 4))
-                choices[i] = negative[choices[i] - 1];
-            if (survey[i][0] == 'T')
-                choices[i] *= -1; 
-            point[0] += choices[i] % 4;
-        }
-        else if (survey[i][0] + survey[i][1] == 'C' + 'F')
-        {
-            if (!(choices[i] / 4))
-                choices[i] = negative[choices[i] - 1];
-            if (survey[i][0] == 'F')
-                choices[i] *= -1;
-            point[1] += choices[i] % 4;
-        }    
-        else if (survey[i][0] + survey[i][1] == 'J' + 'M')
-        {
-            if (!(choices[i] / 4))
-                choices[i] = negative[choices[i] - 1];
-            if (survey[i][0] == 'M')
-                choices[i] *= -1;
-            point[2] += choices[i] % 4;
-        }
-        else if (survey[i][0] + survey[i][1] == 'A' + 'N')
-        {
-            if (!(choices[i] / 4))
-                choices[i] = negative[choices[i] - 1];
-            if (survey[i][0] == 'N')
-                choices[i] *= -1;
-            point[3] += choices[i] % 4;
-        }
-        else
-            return ("Incorrect Character");   
+            return ("Incorrect Choise");
+        j = return_point (survey[i], &choices[i]);
+        if (j == 4)
+            return ("Incorrect Character");
+        point[j] += choices[i] % 4;   
     }
     for (int i = 0; i < 4; i++)
     {
