6ae674d94e34c182197e893ced38ba3e87e094b3
Je Hwang <88884947+JeisaNewbie@users.noreply.github.com>
2022-12-04 22:07:58 +0900

    Create 소수 만들기.c

---
+++ 소수 만들기.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int check_prime (int number)
+{
+    for (int i = 3; i < number; i++)// 짝수를 제외했기 때문에 3부터 시작.
+    {
+        if (!(number % i))
+            return (0);
+    }
+    return (1);
+}
+
+int solution(int nums[], size_t nums_len) {
+    int answer = 0;
+    for (size_t i = 0; i < nums_len - 2; i++)
+    {
+        for (size_t j = i + 1; j < nums_len - 1; j++)
+        {
+            for (size_t k = j + 1; k < nums_len; k++)
+            {
+                if (!((nums[i] + nums[j] + nums[k]) % 2))//짝수는 2로 나눌수 있기 때문에 바로 넘어간다.
+                    continue;
+                else
+                {
+                    if (check_prime(nums[i] + nums[j] + nums[k]))
+                        answer++;                    
+                    continue;
+                }
+            }
+        }
+    }
+    return answer;
+}
