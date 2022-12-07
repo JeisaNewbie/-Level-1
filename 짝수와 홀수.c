0ad50001644b8efd49eb498bf850929bff78b473
Je Hwang <88884947+JeisaNewbie@users.noreply.github.com>
2022-12-07 23:04:35 +0900

    Create 짝수와 홀수.c

---
+++ 짝수와 홀수.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* solution(int num) {
+  char  *odd = "Odd";
+  char  *even = "Even";
+  char  *answer;
+  
+  return answer = num % 2 ? strdup (odd) : strdup (even);
+}
