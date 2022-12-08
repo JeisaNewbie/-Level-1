8a6a90e67948098e99af1a33c9f8d7f9aa670a8d
Je Hwang <88884947+JeisaNewbie@users.noreply.github.com>
2022-12-08 12:27:36 +0900

    Create 정수 제곱근 판별.c

---
+++ 정수 제곱근 판별.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+long long solution(long long n) {
+    
+    for (long long i = 1; i <= n / 2; i++)
+    {
+        if (i * i == n)
+            return (i + 1) * (i + 1);
+    }
+    return n == 1 ? 4 : -1;
+}
